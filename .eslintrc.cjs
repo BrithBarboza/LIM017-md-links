c3dc7586ad680b26a23c26e406badbd23f6bbf50
BrithBarboza <brith.19.04@gmail.com>
2022-05-17 10:57:57 -0500

    none

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,17 @@ module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
+		jest: true,
 	},
-	/* extends: [
-    'standard',
-    'eslint-config-prettier'
-  ], */
+	extends: [
+	    'standard',
+	    'eslint-config-prettier'
+	],
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-	rules: {},
+	rules: {
+
+	},
 };
