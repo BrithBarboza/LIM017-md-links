73eefcbdbf82376b293ee60733cb6c7876617233
BrithBarboza <brith.19.04@gmail.com>
2022-05-17 12:35:40 -0500

    nothing

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import fs from 'fs';
+/* import fs from 'fs'; */
 
 export const getArgPathAbsolute = argPath =>
 	path.isAbsolute(argPath) ? argPath : path.resolve(argPath);
