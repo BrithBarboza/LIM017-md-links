f69ffec86ec17002c6f943b7359baa2ce7a4d864
BrithBarboza <brith.19.04@gmail.com>
2022-05-25 18:01:41 -0500

    list files in directory

---
+++ files/archivo0.md
@@ -0,0 +1,4 @@
+# Markdown Links - archivo0
+
+Aquí se encuentran links que serán útiles para ejecutar las pruebas.
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-// import fs from 'fs';
-// import path from 'path';
-import { getArgPathAbsolute } from './util.js';
+import { getArgPathAbsolute } from './utils.js';
 
 // isAbsolute = Verifica si la ruta es absoluta
 // resolve = Convierte una ruta relativa en una absoluta
 
 const mdLinks = (argPath, options) => {
 	if (!argPath) {
-		// console.log('no hay path');
 		return console.log('Ingrese un path');
 	}
 	return console.log(getArgPathAbsolute(argPath));
